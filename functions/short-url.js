7c92c430a45f13e1d4068c12ab68788d4f6a1178
Matheus Freitas <devel_16@autoavaliar.com.br>
2021-03-22 03:57:45 -0300

    Code optimization and organization with redis

--- functions/short-url.js
+++ functions/short-url.js
@@ -1,3 +1,54 @@
+/**
+ * Serverless Function Requirements
+ */
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host : process.env.REDIS_HOST,
+    port : process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
+});
+
+/**
+ * Creates a random string.
+ * 
+ * @source https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+ * @param {Number} length 
+ * @returns 
+ */
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
+/**
+ * Generates unique random string for a given URL.
+ * 
+ * @param {Number} String length 
+ * @param {String} Destination url (where the user will be redirected) 
+ * @returns {Object} 
+ */
+async function generateUniqueShorterUrl(length, destinationUrl) {
+    let uniqueString = makeid(length);
+    let shorterUrlExists = await redisClient.get(uniqueString);
+
+    if(shorterUrlExists) {
+        return generateUniqueShorterUrl(length);
+    }
+
+    let saveUrl = await redisClient.set(`url_${uniqueString}`, destinationUrl);
+
+    return {
+        "shorterUrl": uniqueString,
+        "destinationUrl": destinationUrl,
+    }
+}
+
 exports.handler = async function(event, context) {
     if(event.httpMethod != 'POST') {
         return {
@@ -9,9 +60,9 @@ exports.handler = async function(event, context) {
         };
     }
 
-    const redis = require('redis');
     const request = JSON.parse(event.body);
-    
+    // const generateUrlShorterVersionKey = await generateUniqueShorterUrl(8, request.url);
+
     return {
         statusCode: 200,
         body: JSON.stringify(event)
